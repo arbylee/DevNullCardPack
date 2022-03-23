f99c177cc0ab36c3f778fd8de24a036d8b159220
Richard Lee <rblee88@gmail.com>
2022-03-23 14:46:14 -0500

    Setup initial scaffolding

---
+++ DevNullCardPack/Cards/Template.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace DevNullCardPack.Cards
+{
+    class Template : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            UnityEngine.Debug.Log($"[{DevNullCardPack.ModInitials}][Card] {GetTitle()} has been setup.");
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+            UnityEngine.Debug.Log($"[{DevNullCardPack.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+            UnityEngine.Debug.Log($"[{DevNullCardPack.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+        }
+
+        protected override string GetTitle()
+        {
+            return "CardName";
+        }
+        protected override string GetDescription()
+        {
+            return "CardDescription";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Effect",
+                    amount = "No",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return DevNullCardPack.ModInitials;
+        }
+    }
+}
---
+++ DevNullCardPack/DevNullCardPack.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using DevNullCardPack.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace DevNullCardPack
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class DevNullCardPack : BaseUnityPlugin
+    {
+        public static DevNullCardPack instance { get; private set; }
+        private const string ModId = "com.arbylee.rounds.DevNullCardPack";
+        private const string ModName = "DevNullCardPack";
+        public const string Version = "0.1.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "DNCP";
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<MyCardName>();
+            instance = this;
+        }
+
+    }
+}
