88e3ecbdbf3061c1f50f0cdc69975f900867ad9b
Richard Lee <rblee88@gmail.com>
2022-04-03 12:10:03 -0500

    Add max size check for grow others

--- DevNullCardPack/MonoBehaviours/GrowOthersEffect.cs
+++ DevNullCardPack/MonoBehaviours/GrowOthersEffect.cs
@@ -8,10 +8,26 @@ namespace DevNullCardPack.MonoBehaviours
 {
     internal class GrowOthersEffect : ReversibleEffect
     {
+        private bool ranOnce = false;
+        public override void OnUpdate()
+        {
+            if (!ranOnce)
+            {
+                Grow();
+            }
+        }
         public void Grow()
         {
             // characterStatModifiers.sizeMultiplier *= 1.1f;
-            base.transform.localScale *= 1.4f;
+            //characterStatModifiersModifier.sizeMultiplier_add = 0.5f;
+            UnityEngine.Debug.Log($"Local scale before {base.transform.localScale}");
+            if (player != null && player.transform.localScale.x < 10)
+            {
+                player.transform.localScale *= 1.4f;
+                UnityEngine.Debug.Log($"Local scale after {base.transform.localScale}");
+                //this.ApplyModifiers();
+                ranOnce = true;
+            }
         }
     }
 }
