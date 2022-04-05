bf0e13540b191c4ec232ec49355f807b86d5c210
Richard Lee <rblee88@gmail.com>
2022-04-05 14:38:42 -0500

    Use sizeMultipler_mult to adjust player size instead of directly manipulating scale

--- DevNullCardPack/MonoBehaviours/GrowOthersAttacher.cs
+++ DevNullCardPack/MonoBehaviours/GrowOthersAttacher.cs
@@ -5,10 +5,14 @@ namespace DevNullCardPack.MonoBehaviours
 {
     internal class GrowOthersAttacher : DealtDamageEffect
     {
-        public override void DealtDamage(Vector2 damage, bool selfDamage, Player damagedPlayer)
+        private const int maxEffects = 5;
+    public override void DealtDamage(Vector2 damage, bool selfDamage, Player damagedPlayer)
         {
-            var effect = damagedPlayer.gameObject.GetOrAddComponent<GrowOthersEffect>();
-            effect.Grow();
+            GrowOthersEffect[] components = damagedPlayer.gameObject.GetComponents<GrowOthersEffect>();
+            if (components.Length < maxEffects)
+            {
+                damagedPlayer.gameObject.AddComponent<GrowOthersEffect>();
+            }
         }
     }
 }
--- DevNullCardPack/MonoBehaviours/GrowOthersEffect.cs
+++ DevNullCardPack/MonoBehaviours/GrowOthersEffect.cs
@@ -8,26 +8,10 @@ namespace DevNullCardPack.MonoBehaviours
 {
     internal class GrowOthersEffect : ReversibleEffect
     {
-        private bool ranOnce = false;
-        public override void OnUpdate()
+        public override void OnStart()
         {
-            if (!ranOnce)
-            {
-                Grow();
-            }
-        }
-        public void Grow()
-        {
-            // characterStatModifiers.sizeMultiplier *= 1.1f;
-            //characterStatModifiersModifier.sizeMultiplier_add = 0.5f;
             UnityEngine.Debug.Log($"Local scale before {base.transform.localScale}");
-            if (player != null && player.transform.localScale.x < 10)
-            {
-                player.transform.localScale *= 1.4f;
-                UnityEngine.Debug.Log($"Local scale after {base.transform.localScale}");
-                //this.ApplyModifiers();
-                ranOnce = true;
-            }
+                characterStatModifiersModifier.sizeMultiplier_mult = 1.4f;
         }
     }
 }
